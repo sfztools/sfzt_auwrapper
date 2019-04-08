b152bd7b5efb57866aaceb9fad21fd1b3a566943
ygrabit <ygrabit@users.noreply.github.com>
2019-04-08 17:25:10 +0200

    vstsdk3613_08_04_2019_build_81

--- usediids.cpp
+++ usediids.cpp
@@ -1,10 +1,10 @@
-//------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 // Project     : VST SDK
 //
-// Category    : Helpers
-// Filename    : public.sdk/source/vst/auwrapper/auwrapper_prefix.pch
-// Created by  : Steinberg, 12/2007
-// Description : VST 3 -> AU Wrapper
+// Category    : Validator
+// Filename    : usediids.cpp
+// Created by  : Steinberg 09.2008
+// Description : Interface symbols file
 //
 //-----------------------------------------------------------------------------
 // LICENSE
@@ -30,9 +30,43 @@
 // BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
 // DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
 // LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
-// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE  OF THIS SOFTWARE, EVEN IF ADVISED
+// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 // OF THE POSSIBILITY OF SUCH DAMAGE.
 //-----------------------------------------------------------------------------
+//#define INIT_CLASS_IID
+// This macro definition modifies the behavior of DECLARE_CLASS_IID (funknown.h)
+// and produces the actual symbols for all interface identifiers.
+// It must be defined before including the interface headers and
+// in only one source file!
+//------------------------------------------------------------------------
+//#define INIT_CLASS_IID
 
-#include <CoreFoundation/CoreFoundation.h>
-#include <CoreAudio/CoreAudio.h>
+#include "pluginterfaces/vst/ivstaudioprocessor.h"
+#include "pluginterfaces/vst/ivsteditcontroller.h"
+#include "pluginterfaces/vst/ivsthostapplication.h"
+#include "pluginterfaces/vst/ivstparameterchanges.h"
+#include "pluginterfaces/vst/ivstpluginterfacesupport.h"
+#include "pluginterfaces/vst/ivstevents.h"
+#include "pluginterfaces/vst/ivstunits.h"
+
+namespace Steinberg {
+DEF_CLASS_IID (Vst::IAttributeList)
+DEF_CLASS_IID (Vst::IAudioProcessor)
+DEF_CLASS_IID (Vst::IEditController)
+DEF_CLASS_IID (Vst::IEditController2)
+DEF_CLASS_IID (Vst::IComponent)
+DEF_CLASS_IID (Vst::IComponentHandler)
+DEF_CLASS_IID (Vst::IConnectionPoint)
+DEF_CLASS_IID (Vst::IEventList)
+DEF_CLASS_IID (Vst::IHostApplication)
+DEF_CLASS_IID (Vst::IMessage)
+DEF_CLASS_IID (Vst::IMidiMapping)
+DEF_CLASS_IID (Vst::IParameterChanges)
+DEF_CLASS_IID (Vst::IParamValueQueue)
+DEF_CLASS_IID (Vst::IPlugInterfaceSupport)
+DEF_CLASS_IID (Vst::IProgramListData)
+DEF_CLASS_IID (Vst::IStreamAttributes)
+DEF_CLASS_IID (Vst::IVst3ToAUWrapper)
+DEF_CLASS_IID (Vst::IUnitData)
+DEF_CLASS_IID (Vst::IUnitInfo)
+}
