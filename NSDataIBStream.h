4ad0429a38b8aad3d823ca398ec7e72874a2f236
redtide <redtid3@gmail.com>
2023-05-24 12:57:26 +0200

    NSDataIBStream IPtr<IAttributeList> patch from @PythonBlue

--- NSDataIBStream.h
+++ NSDataIBStream.h
@@ -8,7 +8,7 @@
 //
 //-----------------------------------------------------------------------------
 // LICENSE
-// (c) 2020, Steinberg Media Technologies GmbH, All Rights Reserved
+// (c) 2023, Steinberg Media Technologies GmbH, All Rights Reserved
 //-----------------------------------------------------------------------------
 // Redistribution and use in source and binary forms, with or without modification,
 // are permitted provided that the following conditions are met:
@@ -68,7 +68,7 @@ public:
 protected:
 	NSData* data;
 	int64 currentPos;
-	HostAttributeList attrList;
+	IPtr<IAttributeList> attrList;
 	bool hideAttributes;
 };
 
